2ccd04cba71966cdec14df256229208a35dc6cbe
aspauldingcode <aspauldingcode@gmail.com>
2025-11-08 12:13:31 -0800

    fix(backend): remove stray 'public' causing Maven compile error in ActiveGame

--- backend/src/main/java/com/onevoneme/model/ActiveGame.java
+++ backend/src/main/java/com/onevoneme/model/ActiveGame.java
@@ -2,7 +2,6 @@ package com.onevoneme.model;
 
 public class ActiveGame {
     public GameUser[] users;
-    public
     public ActiveGame(GameUser user1, GameUser user2) {
         users = new GameUser[] {user1, user2};
     }
